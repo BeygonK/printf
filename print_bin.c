d08facdcf3841cf7786e76bed93be9971719ba2a
BeygonK <kipkoechbeygon@gmail.com>
2023-11-15 01:36:56 -0800

    Updated the print function

--- print_bin.c
+++ print_bin.c
@@ -12,7 +12,7 @@ int printf_bin(va_list val)
 	int i, a = 1, b;
 	unsigned int num = va_arg(val, unsigned int);
 	unsigned int p;
-	
+
 	for (i = 0; i < 32; i++)
 	{
 		p = ((a << (31 - i)) & num);
@@ -31,6 +31,6 @@ int printf_bin(va_list val)
 		count++;
 		_putchar('0');
 	}
-	
+
 	return (count);
 }
